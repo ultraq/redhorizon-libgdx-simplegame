be12a7aa90aacd27ba906c25ca5efe08f9fbbe56
Emanuel Rabina <emanuelrabina@gmail.com>
2025-09-14 17:20:23 +1200

    Get that bucket showing

--- source/nz/net/ultraq/simplegame/SimpleGame.groovy
+++ source/nz/net/ultraq/simplegame/SimpleGame.groovy
@@ -16,14 +16,72 @@
 
 package nz.net.ultraq.simplegame
 
+import nz.net.ultraq.redhorizon.graphics.Image
+import nz.net.ultraq.redhorizon.graphics.Shader
+import nz.net.ultraq.redhorizon.graphics.Window
+import nz.net.ultraq.redhorizon.graphics.opengl.BasicShader
+import nz.net.ultraq.redhorizon.graphics.opengl.OpenGLWindow
+
+import org.joml.Matrix4f
+import picocli.CommandLine
+import picocli.CommandLine.Command
+
 /**
  * Entry point to the simple game example.
  *
  * @author Emanuel Rabina
  */
-class SimpleGame {
+@Command(name = 'libgdx-simple-game')
+class SimpleGame implements Runnable {
+
+	static {
+		System.setProperty('org.lwjgl.system.stackSize', '10240')
+	}
 
 	static void main(String[] args) {
-		println('👋')
+		System.exit(new CommandLine(new SimpleGame()).execute(args))
+	}
+
+	private Window window
+	private Image backkgroundImage
+	private Image bucketImage
+	private Image dropImage
+	private Shader shader
+
+	@Override
+	void run() {
+
+		try {
+			window = new OpenGLWindow(800, 500, 'libGDX Simple Game')
+				.withVSync(true)
+			shader = new BasicShader()
+			backkgroundImage = new Image('background.png', getResourceAsStream('nz/net/ultraq/simplegame/background.png'))
+			bucketImage = new Image('bucket.png', getResourceAsStream('nz/net/ultraq/simplegame/bucket.png'))
+			dropImage = new Image('drop.png', getResourceAsStream('nz/net/ultraq/simplegame/drop.png'))
+
+			var projection = new Matrix4f().setOrthoSymmetric(800, 500, 0, 1)
+			var view = new Matrix4f().setLookAt(
+				400, 250, 1,
+				400, 250, 0,
+				0, 1, 0
+			)
+
+			window.show()
+			while (!window.shouldClose()) {
+				window.withFrame { ->
+					shader.use()
+					shader.setUniform('projection', projection)
+					shader.setUniform('view', view)
+					bucketImage.draw(shader)
+				}
+				Thread.yield()
+			}
+		}
+		finally {
+			dropImage?.close()
+			bucketImage?.close()
+			backkgroundImage?.close()
+			window?.close()
+		}
 	}
 }
